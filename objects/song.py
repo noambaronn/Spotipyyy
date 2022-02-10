d62bd6acd37d57d15a09b810ff9f226b501c5628
noam <noam88888@gmail.com>
2022-02-10 17:04:36 +0200

    deleted the sterilization of song
    dont need to write songs to json files

--- objects/song.py
+++ objects/song.py
@@ -17,26 +17,3 @@ class Song:
         return "id is %s, name is %s, popularity is %s" % (self.song_id, self.name_of_song, self.popularity)
 
 
-class SongSerializer:
-    def serialize(self, song, format):
-        serializer = self._get_serializer(format)
-        return serializer(song)
-
-    def _get_serializer(self, format):
-        if format == 'JSON':
-            return self._serialize_to_json
-        elif format == 'XML':
-            return self._serialize_to_xml
-        else:
-            raise ValueError(format)
-
-    def _serialize_to_json(self, song):
-        if format == 'JSON':
-            song_info = {
-                'id': song.song_id,
-                'name': song.name,
-                'popularity': song.popularity
-            }
-            return json.dumps(song_info)
-        else:
-            raise ValueError(format)
